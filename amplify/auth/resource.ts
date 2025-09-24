c1654436136460868f0ab7ddd6931b7d31eecb59
Luiz Carlos <kakudoluiz@gmail.com>
2025-09-24 12:44:22 -0300

    implementado login e cadastro com o google ou facebook, corrigindo bugs 2

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,19 +1,7 @@
-import { defineAuth, secret } from '@aws-amplify/backend';
+import { defineAuth } from '@aws-amplify/backend';
 
 export const auth = defineAuth({
   loginWith: {
     email: true,
-    externalProviders: {
-      google: {
-        clientId: secret('GOOGLE_CLIENT_ID'),
-        clientSecret: secret('GOOGLE_CLIENT_SECRET'),
-      },
-      facebook: {
-        clientId: secret('FACEBOOK_CLIENT_ID'),
-        clientSecret: secret('FACEBOOK_CLIENT_SECRET'),
-      },
-      callbackUrls: ['http://localhost:4200/'],
-      logoutUrls: ['http://localhost:4200/'],
-    },
   },
 });
